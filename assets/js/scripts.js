01956f86f93c54c7da8e10691a8c7676f5f1968f
Shah-alom <105594703+shahalom8733@users.noreply.github.com>
2022-05-17 14:42:30 +0600

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $('.fa-xmark').click(function(){
+    $('.enters').hide();
+  })
+ $('.fa-magnifying-glass').click(function(){
+    $('.header-icon').slideToggle();
+  })
+
+
+  $(".owl-carousel").owlCarousel({
+  	nav:true,
+  	loop:true,
+  	margin:20,
+  	items:5,
+  	autoplay:true,
+  	navText:['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-angle-right"></i>'],
+  	responsiveClass:true,
+  responsive:{
+        0:{
+            items:2,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:true
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:true
+        }
+    }
+  });
+
+});
